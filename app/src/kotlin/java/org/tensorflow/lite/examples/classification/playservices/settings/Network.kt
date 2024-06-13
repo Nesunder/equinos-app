d3a2a2e8cba5b0e73c9a77ec6f2250a14d0f2802
Agustin Schilling <schillingagustin@gmail.com>
2024-06-12 23:38:03 -0300

    Interaccion con api para la creacion de cuentas de usuario

---
+++ app/src/kotlin/java/org/tensorflow/lite/examples/classification/playservices/settings/Network.kt
@@ -0,0 +1,9 @@
+package org.tensorflow.lite.examples.classification.playservices.settings
+
+class Network {
+    companion object {
+        val URL: String = "https://c4c8-201-235-161-12.ngrok-free.app"
+    }
+
+    // TODO mover llamados a api a esta clase.
+}
