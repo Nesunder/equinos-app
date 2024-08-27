21e2a4a7d9b9b29179bc61e2aaff44e04a97b55c
Agustin Schilling <schillingagustin@gmail.com>
2024-08-26 23:49:51 -0300

    Fix para hacer request a server http local

--- app/src/kotlin/java/com/equinos/settings/Network.kt
+++ app/src/kotlin/java/com/equinos/settings/Network.kt
@@ -7,7 +7,7 @@ import java.lang.ref.WeakReference
 
 class Network {
     companion object {
-        const val BASE_URL: String = "https://9f61-181-168-26-49.ngrok-free.app"
+        const val BASE_URL: String = "http://10.0.2.2:8080"
 
         private var contextRef: WeakReference<Context>? = null
 
